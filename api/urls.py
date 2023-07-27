daee121e9e9c55bd02333f9760c7941da06fd1f8
ivangotovets <gotovets@gmail.com>
2023-07-27 22:11:59 +0300

    доб ручку со случайным сообщением

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .viewsets import MessageViewset
+from .viewsets import MessageViewset, RandomMessageView
 
 
 router = DefaultRouter()
-router.register('messages', MessageViewset)
+router.register('messages', MessageViewset, basename='messages')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v1/random/', RandomMessageView.as_view())
 ]
--- api/viewsets.py
+++ api/viewsets.py
@@ -1,4 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from random import randint
 
 from .models import Message
 from .serializers import MessageSerialiser
@@ -7,3 +12,12 @@ from .serializers import MessageSerialiser
 class MessageViewset(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerialiser
+
+
+class RandomMessageView(APIView):
+    def get(self):
+        last_obj = Message.objects.latest('pub_date')
+        number = randint(1, last_obj.id)
+        message = get_object_or_404(Message, id=number)
+        response = {'random_message': message.text}
+        return Response(response)
